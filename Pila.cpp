a454a59ab99aba7261a5738cbe1822ae97d0b72a
a2025110138mss <a2025110138@estudiantes.upsa.edu.bo>
2026-04-13 13:26:38 -0400

    Create Pila.cpp

---
+++ Pila.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "Pila.h"
+using namespace std;
+
+Pila::Pila()
+{
+    cima = -1;
+}
+
+bool Pila::Apilar(int& elemento)
+{
+    if (cima == MAX - 1)
+    {
+        cout << "Desbordamiento de Pila (Overflow)" << endl;
+        return false;
+    }
+    else
+    {
+        cima++;
+        pila[cima] = elemento;
+        return true;
+    }
+}
+
+bool Pila::Desapilar()
+{
+    if (cima == -1)
+    {
+        cerr << "Underflow: pila vacia" << endl;
+        return false;
+    }
+    else
+    {
+        cima--;
+        return true;
+    }
+}
+
+bool Pila::CimaPila(int& elemento)
+{
+    if (PilaVacia())
+    {
+        cerr << "Pila vacia (underflow)" << endl;
+        return false;
+    }
+    else
+    {
+        elemento = pila[cima]; 
+        return true;
+    }
+}
+
+void Pila::VerPila()
+{
+    for (int i = 0; i <= cima; i++)
+        cout << pila[i] << endl;
+}
+
+bool Pila::PilaVacia()
+{
+    return cima == -1;
+}
+
+void Pila::LimpiarPila()
+{
+    cima = -1;
+}
+
+bool Pila::Iguales(Pila p)
+{
+     
+    return false;
+}
